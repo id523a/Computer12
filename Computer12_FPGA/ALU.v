5a76c9199f75d5a6381b2f650869e1ecd4a7aa6a
Edward Giles <edward@giles.net.au>
2020-08-15 15:16:36 +0800

    Implemented conditional ops in ALU

--- Computer12_FPGA/ALU.v
+++ Computer12_FPGA/ALU.v
@@ -8,15 +8,16 @@ module ALU(
 	output [3:0] flg_out
 );
 	// Flag outputs
-	reg Z_out;
-	reg S_out;
-	reg K_out;
-	reg V_out;
-	reg P_out;
+	reg Z_out; // flag[0] = zero flag
+	reg S_out; // flag[1] = sign flag
+	reg K_out; // flag[2] = carry flag
+	reg V_out; // flag[3] = overflow flag
+	reg P_out; // flag[4] = predicate flag
 	assign flg_out = {P_out, V_out, K_out, S_out, Z_out};
-	// Handle result and carry flag
+	
+	// Compute arithmetic/logic operation, and carry flag (Q, K)
 	wire K_in = flg_in[2];
-	always @(*) begin
+	always @(*) begin : compute_operation_carry
 		K_out = K_in;
 		case (operation)
 			5'h01: /*AND*/ Q = A & B;
@@ -37,9 +38,10 @@ module ALU(
 			default: /*MOV*/ Q = B;
 		endcase
 	end
-	// Handle zero and sign flags
+	
+	// Compute zero and sign flags (Z, S)
 	wire result_zero = (Q == 0);
-	always @(*) begin
+	always @(*) begin : compute_zero_sign
 		if (operation == 5'h00 || operation[4] == 1'b1) begin
 			Z_out = flg_in[0];
 			S_out = flg_in[1];
@@ -49,8 +51,9 @@ module ALU(
 			S_out = Q[11];
 		end
 	end
-	// Handle overflow flag
-	always @(*) begin
+	
+	// Compute overflow flag (V)
+	always @(*) begin : compute_overflow
 		if (operation[3:2] == 2'b00 || operation[4] == 1'b1) begin
 			V_out = flg_in[3];
 		end
@@ -58,13 +61,43 @@ module ALU(
 			V_out = (A[11] & B[11] & ~Q[11]) | (~A[11] & ~B[11] & Q[11]);
 		end
 	end
-	// Handle predicate
-	always @(*) begin
+	
+	// Compute condition based on flags
+	reg cond_value;
+	always @(*) begin : compute_cond_value
+		case (condition)
+			4'h0: cond_value = flg_in[0]; // Z flag / equal
+			4'h1: cond_value = flg_in[1]; // S flag
+			4'h2: cond_value = flg_in[2]; // K flag / unsigned a<b
+			4'h3: cond_value = flg_in[3]; // V flag
+			// Reserved: 4'h4 - 4'h7
+			4'h8: cond_value = ~flg_in[0] & ~flg_in[2]; // Z clear and K clear / unsigned a>b
+			4'h9: cond_value = flg_in[1] ^ flg_in[3]; // S xor V / signed a<b
+			4'ha: cond_value = ~flg_in[0] & ~(flg_in[1] ^ flg_in[3]); // signed a>b
+			// Reserved: 4'hb - 4'he
+			default: cond_value = 1'b1;
+		endcase
+	end
+	
+	// Compute predicate flag (P)
+	// operation[4] indicates that the operation modifies the condition
+	// operation[3] indicates whether the condition is (0) based on flags or (1) result_zero
+	// operation[2] indicates whether to invert the value
+	// operation[1:0] indicates how this is combined with the existing value of the P flag
+	wire cond_op3 = operation[3] ? result_zero : cond_value;
+	wire cond_op2 = operation[2] ^ cond_op3;
+	wire P_in = flg_in[4];
+	always @(*) begin : compute_p_flag
 		if (operation[4] == 1'b0) begin
-			P_out = flg_in[4];
+			P_out = P_in;
 		end
 		else begin
-			P_out = 1'b0;
+			case (operation[1:0])
+				2'b00: P_out = cond_op2;
+				2'b01: P_out = P_in ^ cond_op2;
+				2'b10: P_out = P_in & cond_op2;
+				2'b11: P_out = P_in | cond_op2;
+			endcase
 		end
 	end
 endmodule
