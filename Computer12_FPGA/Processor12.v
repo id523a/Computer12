2a591ef5d8e2a1db6882261bf23764684e8171e5
Edward Giles <edward@giles.net.au>
2020-08-18 21:59:05 +0800

    Allowed processor to fetch first words of instructions from memory

--- Computer12_FPGA/Processor12.v
+++ Computer12_FPGA/Processor12.v
@@ -4,9 +4,19 @@ module Processor12(
 	input [23:0] irq,
 	input [11:0] data_in,
 	output [11:0] data_out,
-	output [23:0] address
+	output [23:0] address,
+	output reg [2:0] state
 );
-	reg [2:0] state;
+	reg [23:0] PC;
+	reg [11:0] instr_store;
+	wire [11:0] instr;
+	wire instr_has_immediate;
+	
+	InstructionDecoder(
+		.instr(instr),
+		.has_immediate(instr_has_immediate)
+	);
+	
 	always @(posedge clk or negedge rst) begin : state_counter
 		if (!rst) begin
 			state <= 3'b000;
@@ -20,24 +30,50 @@ module Processor12(
 				state <= 3'b001;
 		end
 	end
-	InstructionDecoder decoder(
-		.instr(data_in),
-		.dest_reg(data_out[4:0]),
-		.src_reg(data_out[9:5]),
-		.alu_cond(address[3:0])
-	);
+
+	assign address = state[0] ? PC : 24'hxxxxxx;
+	assign instr = state[1] ? data_in : instr_store;
+	always @(posedge clk or negedge rst) begin : instruction_fetch
+		if (!rst) begin
+			PC <= 0;
+			instr_store <= 0;
+		end
+		else begin
+			if (state[0]) begin
+				PC <= PC + 1;
+			end
+			if (state[1]) begin
+				instr_store <= data_in;
+			end
+		end
+	end
+	assign data_out = instr;
 endmodule
 
 `timescale 1ns/1ps
 module Processor12_test();
 	reg clk;
 	reg rst;
+	reg [11:0] data_in;
 	wire [11:0] data_out;
 	wire [23:0] address;
-	Processor12 DUT(clk, rst, 24'b0, 12'b0, data_out, address);
+	wire [2:0] state;
+	
+	always @(posedge clk) begin
+		data_in <= address[11:0] | 12'h800;
+	end
+	Processor12 DUT(
+		.clk(clk),
+		.rst(rst),
+		.irq(24'b0),
+		.data_in(data_in),
+		.data_out(data_out),
+		.address(address),
+		.state(state)
+	);
 	initial begin
 		clk = 0;
-		repeat (1000) begin
+		repeat (100) begin
 			#25
 			clk = 1;
 			#25
