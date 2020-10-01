8596ceed9175b95053e76c63e6c78b6204a742c6
Edward Giles <edward@giles.net.au>
2020-10-01 13:56:28 +0800

    Used re_lexer to create a lexer for assembly language

--- Assembler12/Assembler12/__main__.py
+++ Assembler12/Assembler12/__main__.py
@@ -1,55 +1,75 @@
-def parse_literal(num_str):
-    # Number format : # <base> <sign> <digits>
-    # Base is optional, defaults to decimal
-    # Sign is optional, defaults to positive
-    # Bb = binary, Oo = octal, Dd = decimal, Hh = hex
-    # For example: "#h-2c" is -44
-    iter_str = iter(num_str)
-    sign = 1
-    result = 0
-    result_valid = False
-    try:
-        # Make sure first character is '#'
-        ch = next(iter_str)
-        if (ch != '#'):
-            return None
-        ch = next(iter_str)
-        # Attempt to read base
-        base = parse_literal.number_bases.get(ch)
-        if base is None:
-            base = 10
-        else:
-            ch = next(iter_str)
-        # Attempt to read sign
-        if ch == '+':
-            sign = 1
-            ch = next(iter_str)
-        elif ch == '-':
-            sign = -1
-            ch = next(iter_str)
-        # Read digits
-        while True:
-            if ch != '_': # Ignore underscores in digit string
-                digit_value = parse_literal.digit_values.get(ch, -1)
-                if (digit_value < 0 or digit_value >= base):
-                    return None
-                result *= base
-                result += digit_value
-                result_valid = True
-            ch = next(iter_str)
-    except StopIteration:
-        return sign * result if result_valid else None
+from enum import Enum
+import re
+from re_lexer import Lexer, LexError
+from asm_state import *
 
-parse_literal.number_bases = {
+class TokenType(Enum):
+    NEWLINE = 0
+    END_STATEMENT = 1
+    NUMBER = 2
+    REG = 3
+    DOUBLE_REG = 4
+    IDENTIFIER = 5
+    COLON = 6
+    COMMA = 7
+    LEFT_BRACKET = 8
+    RIGHT_BRACKET = 9
+    AUTO_INCREMENT = 10
+    AUTO_DECREMENT = 11
+    PLUS = 12
+
+number_bases = {
+    '': 10,
     'B': 2, 'b': 2,
     'O': 8, 'o': 8,
     'D': 10, 'd': 10,
     'H': 16, 'h': 16
 }
 
-parse_literal.digit_values = {
+digit_values = {
     '0': 0, '1': 1, '2': 2, '3': 3, '4': 4,
     '5': 5, '6': 6, '7': 7, '8': 8, '9': 9,
     'A': 10, 'B': 11, 'C': 12, 'D': 13, 'E': 14, 'F': 15,
     'a': 10, 'b': 11, 'c': 12, 'd': 13, 'e': 14, 'f': 15
 }
+
+def parse_number(match):
+    base = number_bases[match.group('number_base')]
+    sign = 1
+    if match.group('number_sign') == '-':
+        sign = -1
+    result = 0
+    for digit_char in match.group('number_digits'):
+        if digit_char == '_':
+            continue
+        digit = digit_values.get(digit_char)
+        if digit is not None and digit < base:
+            result *= base
+            result += digit
+        else:
+            raise ValueError(f'Invalid digit in base {base}: {digit_char!r}')
+    return sign * result
+
+def extract_name(match):
+    return match.group()
+
+assembly_lexer = Lexer([
+    (r"\n", TokenType.NEWLINE),
+    (r";", TokenType.END_STATEMENT),
+    (r"#(?P<number_base>[bodh]?)(?P<number_sign>[+-]?)(?P<number_digits>[0-9a-z_]+)", TokenType.NUMBER, parse_number),
+    (r"\b(?:[ABCI]P[LH]|[ABCDEFGZ])\b", TokenType.REG, extract_name),
+    (r"\b[ABCI]P\b", TokenType.DOUBLE_REG, extract_name),
+    (r"\w+", TokenType.IDENTIFIER, extract_name),
+    (r":", TokenType.COLON),
+    (r",", TokenType.COMMA),
+    (r"\[", TokenType.LEFT_BRACKET),
+    (r"\]", TokenType.RIGHT_BRACKET),
+    (r"\+\+", TokenType.AUTO_INCREMENT),
+    (r"\-\-", TokenType.AUTO_DECREMENT),
+    (r"\+", TokenType.PLUS),
+    (r"\s+", None) # Ignore whitespace by default
+], re.IGNORECASE)
+
+with open('../test.a12', 'r') as f:
+    for token_type, value in assembly_lexer.tokenize(f):
+        print(token_type.name.rjust(15), "" if value is None else str(value))
